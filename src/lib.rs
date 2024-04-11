04162e3219697c178e73767b63bc4561d1e1bf4a
ManishThulung <manish.thulung58@gmail.com>
2024-04-11 13:18:45 +0545

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,43 @@ pub struct Config{
 }
 
 impl Config {
-  pub fn build(args: &Vec<String>) -> Result<Config, &str> {
-      if args.len() < 3 {
-        return Err("not enough args");
-      }
-      let query = &args[1];
-      let file_path = &args[2];
+  pub fn build(
+    mut args: impl Iterator<Item = String>,
+  ) -> Result<Config, &'static str> {
+    args.next();
 
-      let ignore_case = env::var("IGNORE_CASE").is_ok();
-      
-      Ok(Config {
-        query: query.to_string(),
-        file_path: file_path.to_string(),
-        ignore_case
-      })
-   }
+    let query: String = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+
+    let file_path: String = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file path"),
+    };
+
+    let ignore_case: bool = env::var("IGNORE_CASE").is_ok();
+
+    Ok(Config{
+      query,
+      file_path,
+      ignore_case
+    })
+  }
+  // pub fn build(args: &Vec<String>) -> Result<Config, &str> {
+  //   if args.len() < 3 {
+  //     return Err("not enough args");
+  //   }
+  //   let query = &args[1];
+  //   let file_path = &args[2];
+  //   let ignore_case = env::var("IGNORE_CASE").is_ok();
+    
+  //   Ok(Config {
+  //     query: query.to_string(),
+  //     file_path: file_path.to_string(),
+  //     ignore_case
+  //   })
+  // }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
@@ -43,13 +65,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query:&str, contents: &'a str) -> Vec<&'a str> {
-  let mut result = Vec::new();
-  for line in contents.lines() {
-    if line.contains(query) {
-      result.push(line);
-    }
-  }
-  result
+  // let mut result = Vec::new();
+  // for line in contents.lines() {
+  //   if line.contains(query) {
+  //     result.push(line);
+  //   }
+  // }
+  // result
+
+  contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query:&str, contents: &'a str) -> Vec<&'a str> {
